560a1e3777e94c1b037ac2776707e9067a68de28
ChenCoin <chencoin.cn@gmail.com>
2024-04-25 22:18:41 +0800

    catfish fix

--- catfish/flutter_service_worker.js
+++ catfish/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a9aba63c4d2d33142ae8e7496b4c81d0",
-"/": "a9aba63c4d2d33142ae8e7496b4c81d0",
-"main.dart.js": "16846bbdc8ecedc46503332cee60ea5d",
+"index.html": "6ffa85fb92d1cf86081a419c2d5f8d22",
+"/": "6ffa85fb92d1cf86081a419c2d5f8d22",
+"main.dart.js": "31fab0f86b721665e5b674349fdf675b",
 "manifest.json": "b0b687cad9ec1da9e19ca926aa523e40",
 "version.json": "ae07041ee9c4343b8994305fe8328f38"};
 // The application shell files that are downloaded before a service worker can
