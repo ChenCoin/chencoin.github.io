039986b54fb8d2bf5901bec59f870b21ba12ea87
ChenCoin <chencoin.cn@gmail.com>
2024-04-29 22:29:47 +0800

    dogcart v2.0.0

--- dogcart/flutter_service_worker.js
+++ dogcart/flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ffc890ef46f29ad4501302fb84ca8d13",
-"/": "ffc890ef46f29ad4501302fb84ca8d13",
-"main.dart.js": "f06ce31e867525b26de0acbccb51f347",
+"index.html": "b1cbe6f635615d0f90cc9a836307d328",
+"/": "b1cbe6f635615d0f90cc9a836307d328",
+"main.dart.js": "e555e44e630c0f47982db9a52d07f6c1",
 "manifest.json": "f7a2d0cc97bbf6d3cdf4be1b42d61c59",
 "version.json": "53ae6ee893bbb65f7d7b9ff995954a34"};
 // The application shell files that are downloaded before a service worker can
