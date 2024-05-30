27188ce414b0d35b1d4336bcd86568cf9ec7cb55
ChenCoin <chencoin.cn@gmail.com>
2024-05-30 22:04:13 +0800

    loading

--- dogcart/flutter_bootstrap.js
+++ dogcart/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2218900688"
+    serviceWorkerVersion: "1002651761"
   }
 });
